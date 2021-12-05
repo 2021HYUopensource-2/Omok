b7385c09743f574636f6e865f00d0067aad6d4ca
21yuyu99 <91944385+21yuyu99@users.noreply.github.com>
2021-12-06 01:04:06 +0900

    Update login_new_version.py

--- login_new_version.py
+++ login_new_version.py
@@ -1,29 +1,34 @@
 import csv
 class Login:
-    member=[]
     login_member=[]   
-    def f_register(self): #파일에 리스트로 저장
+    def f_register(self,member): #파일에 리스트로 저장
         t=open('member.csv','a',newline='')
         wr = csv.writer(t)
-        wr.writerow(self.member)
-        print("회원가입 완료",end="")
+        wr.writerow(member)
+        print("회원가입 완료")
         t.close()
 
     def check(self,check_id,what): #0은 중복 확인 /1은 아이디 정보 확인
         with open("member.csv","r") as t:
             rdr = csv.reader(t)
+            cnt=0
             for line in rdr:
-                if(line[0]==check_id):
+                if(line[1]==check_id):
                     if(what==0):
-                        return 1
+                        return -1
                     else:
                         return line
-            return 0
+                cnt+=1
+            if what==1:
+                return 0
+            else:
+                return cnt
         
     def save_member(self,player1,player2):
         self.login_member.append(player1)
         self.login_member.append(player2)
         return self.login_member
+
     def control(self):
         print("환영합니다:)")
         cnt=0
@@ -37,14 +42,14 @@ class Login:
                 c=self.check(search_id,1)
                 if (cnt==0 and c!=0):
                     search_pw=input("비밀번호를 입력해주세요: ")
-                    if c[1]==search_pw:
+                    if c[2]==search_pw:
                         cnt+=1
                         player1=c
                         print("player1 로그인 성공")
                         continue
                 elif (cnt==1 and search_id!=player1[0]) :
                     search_pw=input("비밀번호를 입력해주세요: ")
-                    if c[1]==search_pw:
+                    if c[2]==search_pw:
                         player2=c
                         print("player2 로그인 성공 \n")
                         print("---------------")
@@ -58,12 +63,13 @@ class Login:
                     continue
             if user_input==2:
                 new_id=input("아이디를 입력해주세요: ")
-                while(self.check(new_id,0)==1):
+                c=self.check(new_id,0)
+                while(c==-1):
                     new_id=input("이미 있는 아이디입니다.\n아이디를 다시 입력해주세요: ")
+                    c=self.check(new_id,0)
                 new_pw=input("비밀먼호를 입력해주세요: ")
                 while(len(new_pw)<8):
                     new_pw=input("비밀번호를 8자 이상이어야합니다. 비밀번호를 다시 입력해주세요: ")
-                self.member+=[new_id,new_pw,0,0] #[아이디,비밀번호,이긴횟수,진 횟수]
-                self.f_register()
+                self.f_register([c,new_id,new_pw,0,0]) #[번호,아이디,비밀번호,이긴횟수,진 횟수]
             else:
                 return 0
