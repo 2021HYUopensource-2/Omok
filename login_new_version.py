71c52cf0c0a20b263dc2a4f44c1698cf3711476c
21yuyu99 <91944385+21yuyu99@users.noreply.github.com>
2021-12-05 18:30:16 +0900

    Update login_new_version.py

--- login_new_version.py
+++ login_new_version.py
@@ -1,7 +1,7 @@
 import csv
 class Login:
     member=[]
-        
+    login_member=[]   
     def f_register(self): #파일에 리스트로 저장
         t=open('member.csv','a',newline='')
         wr = csv.writer(t)
@@ -17,11 +17,16 @@ class Login:
                     if(what==0):
                         return 1
                     else:
-                        return line[1]
+                        return line
             return 0
-                    
+        
+    def save_member(self,player1,player2):
+        self.login_member.append(player1)
+        self.login_member.append(player2)
+        return self.login_member
     def control(self):
         print("환영합니다:)")
+        cnt=0
         while(1):
             print("---------------")
             print("1.로그인\n2.회원가입\n3.나가기\n메뉴를 선택하세요: ",end="")
@@ -30,15 +35,26 @@ class Login:
             if user_input==1:
                 search_id=input("아이디를 입력해주세요: ")
                 c=self.check(search_id,1)
-                if(c!=0):
+                if (cnt==0 and c!=0):
                     search_pw=input("비밀번호를 입력해주세요: ")
-                    if(c==search_pw):
-                        print("로그인 성공!\n게임을 시작합니다.")
+                    if c[1]==search_pw:
+                        cnt+=1
+                        player1=c
+                        print("player1 로그인 성공")
+                        continue
+                elif (cnt==1 and search_id!=player1[0]) :
+                    search_pw=input("비밀번호를 입력해주세요: ")
+                    if c[1]==search_pw:
+                        player2=c
+                        print("player2 로그인 성공 \n")
+                        print("---------------")
+                        print("게임을 시작합니다.")
+                        return self.save_member(player1,player2)
                     else:
                         print("비밀번호가 틀립니다.")
                         continue
                 else:
-                    print("존재하지 않는 아이디입니다.")
+                    print("잘못된 아이디입니다.")
                     continue
             if user_input==2:
                 new_id=input("아이디를 입력해주세요: ")
@@ -51,6 +67,3 @@ class Login:
                 self.f_register()
             else:
                 return 0
-        
-login=Login()
-login.control()
