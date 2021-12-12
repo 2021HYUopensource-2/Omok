b3d1f57405a048897550f346f45ab414bef2bbe6
depave <92785124+depave@users.noreply.github.com>
2021-12-12 18:45:34 +0900

    add rank system

--- omok_game/login.py
+++ omok_game/login.py
@@ -34,7 +34,7 @@ class Login:
         cnt=0
         while(1):
             print("---------------")
-            print("1.로그인\n2.회원가입\n3.나가기\n메뉴를 선택하세요: ",end="")
+            print("1.로그인\n2.회원가입\n3.랭킹\n4.나가기\n메뉴를 선택하세요: ",end="")
             ########################예외처리하기##########################
             user_input=int(input())
             print("---------------")
@@ -46,7 +46,7 @@ class Login:
                     if c[2]==search_pw:
                         cnt+=1
                         player1=c
-                        print("player1 로그인 성공")
+                        print("\nplayer1 로그인 성공")
                         continue
                     else:
                         print("비밀번호가 틀립니다.")
@@ -75,6 +75,26 @@ class Login:
                 while(len(new_pw)<8):
                     new_pw=input("비밀번호를 8자 이상이어야합니다. 비밀번호를 다시 입력해주세요: ")
                 self.f_register([c,new_id,new_pw,0,0]) #[번호,아이디,비밀번호,이긴횟수,진 횟수]
+            if user_input == 3:
+                data = []
+                with open("member.csv","r") as t:
+                    rdr = csv.reader(t)
+                    for line in rdr:
+                        data += [[line[3],line[4],line[1]]] #( win , lose , id )
+                data.sort(reverse=True)
+                print("\t\t Rank   top5")
+                if(len(data)>0):
+                    print("1. " + data[0][2] + "\t\t\t ( win / lose : " + data[0][0] + " / " + data[0][1] + " )")
+                    if(len(data)>1):
+                        print("2. " + data[1][2] + "\t\t\t ( win / lose : " + data[1][0] + " / " + data[1][1] + " )")
+                        if(len(data)>2):
+                            print("3. " + data[2][2] + "\t\t\t\t ( win / lose : " + data[2][0] + " / " + data[2][1] + " )")
+                            if(len(data)>3):
+                                print("4. " + data[3][2] + "\t\t\t\t ( win / lose : " + data[3][0] + " / " + data[3][1] + " )")
+                                if(len(data)>4):
+                                    print("5. " + data[4][2] + "\t\t\t\t ( win / lose : " + data[4][0] + " / " + data[4][1] + " )")
+                else:
+                    print('\n' + "No one challenged! Start the game and record your score!")
             else:
                 return 0
     
